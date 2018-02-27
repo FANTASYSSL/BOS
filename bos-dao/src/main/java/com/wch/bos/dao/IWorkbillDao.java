055fb4e14af7f416be7fe673fe523bf5408a0136
FANTASY <platofantasy@163.com>
2018-02-27 22:11:24 +0800

    quartz、workbill

---
+++ bos-dao/src/main/java/com/wch/bos/dao/IWorkbillDao.java
@@ -0,0 +1,8 @@
+package com.wch.bos.dao;
+
+import com.wch.bos.dao.impl.base.IBaseDao;
+import com.wch.bos.domain.Workbill;
+
+public interface IWorkbillDao extends IBaseDao<Workbill> {
+
+}
---
+++ bos-dao/src/main/java/com/wch/bos/dao/impl/WorkbillDaoImpl.java
@@ -0,0 +1,11 @@
+package com.wch.bos.dao.impl;
+
+import org.springframework.stereotype.Repository;
+
+import com.wch.bos.dao.IWorkbillDao;
+import com.wch.bos.dao.impl.base.impl.BaseDaoImpl;
+import com.wch.bos.domain.Workbill;
+@Repository
+public class WorkbillDaoImpl extends BaseDaoImpl<Workbill> implements IWorkbillDao{
+
+}
